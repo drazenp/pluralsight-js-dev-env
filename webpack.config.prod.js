f9f9426c90456bfc3db62c2916c3d087a5711055
Drazen Pupovac <drazen.pupovac@kneat.com>
2017-10-26 16:06:51 +0100

    Make bundle.js inject dinamicaly to the page #6

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   devtool: 'source-map',
@@ -13,8 +14,26 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-     // Minify JS
-     new webpack.optimize.UglifyJsPlugin()
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
+
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
